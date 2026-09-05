0b0cae6087df32922a0be94e2d46f96c34fb9804
salmanfaris11 <salmanfariskappoor@gmail.com>
2026-09-06 00:55:25 +0530

    Practice 2

---
+++ nestedobj.js
@@ -0,0 +1,17 @@
+let employee = {
+
+    Name: "Salman",
+    Age: 23,
+    Salary: 30000,
+
+    Address: {
+        State: "Kerala",
+        District: "Malappuram",
+        City: "Perinthalmanna",
+        Ph: 9877899000
+    }
+
+};
+
+console.log(employee.Address.City);
+
