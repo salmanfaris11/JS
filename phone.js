6f89c5f51169fe6d588fabba6ea6f8866f89e40c
salmanfaris11 <salmanfariskappoor@gmail.com>
2026-09-07 01:16:51 +0530

    Practice 2

---
+++ phone.js
@@ -0,0 +1,10 @@
+let phone = new Object ();
+    phone.name = "S26 Ultra";
+    phone.price = 130000;
+    phone.year = 2026;
+
+for (let x in phone){
+    console.log(x+ " : " +phone[x]);
+    
+}
+
